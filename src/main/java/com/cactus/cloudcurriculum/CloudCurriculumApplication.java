f73c7a0079b767ff10733fd6d1c81286c5bf8fa4
cactus <neel0108p@gmail.com>
2023-07-02 16:15:05 -0600

    Clean Up.

--- src/main/java/com/cactus/cloudcurriculum/CloudCurriculumApplication.java
+++ src/main/java/com/cactus/cloudcurriculum/CloudCurriculumApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class CloudCurriculumApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(CloudCurriculumApplication.class, args);
 	}
 
 }
