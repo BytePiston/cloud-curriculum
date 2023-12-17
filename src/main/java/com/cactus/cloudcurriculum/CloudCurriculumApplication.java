ce39cb0da83725a5f4726495c8c297521767fc2f
cactus <neel0108p@gmail.com>
2023-12-17 16:49:58 -0700

    New Project added: https://www.youtube.com/watch?v=Nv2DERaMx-4

---
+++ src/main/java/com/cactus/cloudcurriculum/CloudCurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.cactus.cloudcurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCurriculumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cactus/cloudcurriculum/CloudCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactus.cloudcurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
